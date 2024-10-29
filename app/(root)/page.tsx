e539188680ecad4383b7619c4dd003f86b043b9a
Sifan23 <sifanyg123@gmail.com>
2024-10-29 04:30:20 +0300

    home page updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,47 @@
-import { Button } from '@/components/ui/button'
+
+import AddDocumentBtn from '@/components/AddDocumentBtn'
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser()
+  if (!clerkUser) redirect('/sign-in')
+
+  const documents = []
   return (
-    <Button>Click Me</Button>
+    <main className='home-container'>
+      <Header className='sticky top-0 left-0'>
+          <div className='flex items-center gap-2 lg:gap-4'>
+              Notification
+              <SignedIn>
+                <UserButton/>
+              </SignedIn>
+          </div>
+      </Header>
+      {
+        documents.length > 0 ? (
+            <div></div>
+        ) : (
+            <div className='document-list-empty'>
+                <Image
+                src="/assets/icons/doc.svg"
+                alt="document"
+                height={40}
+                width={40}
+                className='mx-auto'
+                />
+
+                <AddDocumentBtn
+                userId={clerkUser?.id}
+                email={clerkUser?.emailAddresses[0]?.emailAddress}
+                />
+            </div>
+        )}
+    </main>
   )
 }
 
