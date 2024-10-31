a26365b03c2389e878bb49d18210b4264b51104c
Sifan23 <sifanyg123@gmail.com>
2024-10-31 11:37:45 +0300

    rendering active collaborators

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,31 +1,35 @@
-'use client'
-import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+"use client";
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+import ActiveCollaborators from "./ActiveCollaborators";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({roomId, roomMetaData}: CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
-    <ClientSideSuspense fallback={<div>Loading…</div>}>
-      <div className='collaborative-room'>
-      <Header>
-        <div className='flex w-fit items-center justify-center'>
-          <p className='document-title'>Title</p>
+    <RoomProvider id={roomId}>
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center">
+              <p className="document-title">Title</p>
+            </div>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+          <Editor />
         </div>
-        <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </Header>
-      <Editor/>
-      </div>
-    </ClientSideSuspense>
-  </RoomProvider>
-  )
-}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
 
-export default CollaborativeRoom
+export default CollaborativeRoom;
